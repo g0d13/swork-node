791b8c44f5ca578563226c6bd8dea4b669daca99
Juan Diego Parra Mendez <g0d13@outlook.com>
2021-04-26 17:55:30 -0500

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
-import { LogsModule } from './logs/logs.module';
 import { MachinesModule } from './machines/machines.module';
-import { RequestsModule } from './requests/requests.module';
 import { AuthModule } from './auth/auth.module';
 import { RepairsModule } from './repairs/repairs.module';
 import { NotifyModule } from './notify/notify.module';
@@ -22,9 +20,7 @@ import { NotifyModule } from './notify/notify.module';
     NotifyModule,
     UsersModule,
     CategoriesModule,
-    LogsModule,
     MachinesModule,
-    RequestsModule,
     AuthModule,
     RepairsModule,
   ],
